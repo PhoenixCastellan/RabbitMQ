db0e629dce7b89ccc6921e11188933d2162d7a92
吕日辉 <lv.rihui@ronghere.com>
2017-06-04 22:19:38 +0800

    借用官网完成一个最简单的生产者和消费者demon。

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                do
+                {
+                    string message = Console.ReadLine();
+                    if (message.Equals("exit"))
+                    {
+                        break;
+                    }
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                } while (true);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
